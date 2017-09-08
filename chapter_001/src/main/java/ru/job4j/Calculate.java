e60e979e8428afc1889622566cd19158609cbaaf
SirenOfOludeniz <ekaterina22_01_94@mail.ru>
2017-09-08 16:57:13 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j;
+
+/**Class Calculate для вычислеия арифметический операций+ - * / ^.
+ *@author edyachkova
+ *@since 08.09.2017
+ *version 1
+ */
+
+	
 public class Calculate{
-public static void main(String[] args){
-System.out.println("Hello world");
+	/**главный метод. Точка запуска программы
+	*не возвращает никаких значений так как void
+	*выводит на экран Hello world
+	*/
+	public static void main(String[] args){
+		System.out.println("Hello world");
 }
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+*Package for calculate task.
+*@author Ekaterina Dyachkova(mailto:ekaterina22_01_94@mail.ru)
+*@vesion$1$
+*since 08.09.2017
+*/
+package ru.job4j; 
