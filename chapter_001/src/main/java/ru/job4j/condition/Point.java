9cd14a8fc713620b63d830c10aa7dc45600fa18a
SirenOfOudeniz <ekaterina22_01_94@mail.ru>
2017-09-13 23:06:02 +0300

    commit class Point

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,28 +1,28 @@
 package ru.job4j.condition;
  
 public class Point {
-   private int x;
-   private int y;
- 
-   public  Point(int x, int y) {
-      this.x = x;
-      this.y = y;
-  }
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+
+    public boolean is(int a, int b) {
+
+            return (this.y == a * this.x + b);
+        }
+
+    }
+
   
-  public int getX() {
-      return this.x;
-  }
- 
-  public int getY() {
-     return this.y;
-  }
-  }
- class Point {
-  public boolean is(int a, int b) {
-  if(this.y == a*this.x + b) {
-		return true;
-  }
-  
-  }
-  
-}
