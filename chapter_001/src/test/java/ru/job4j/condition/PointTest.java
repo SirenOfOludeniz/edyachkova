3b93e91b513a9dacee8727e9da4e376338e2ee47
SirenOfOudeniz <ekaterina22_01_94@mail.ru>
2017-09-15 21:29:26 +0300

    commit Counter & CounterTest

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -1,11 +1,17 @@
-package tu.job4j.condition;
+package ru.job4j.condition;
+import org.junit.Test;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 
 public class PointTest {
     @Test
     public void whenPointOnLineThenTrue() {
         //create of new point.  
         Point a = new Point(1, 1);
-        // execute method - is and get result;
+        /* execute method - is and get result; */
         boolean rsl = a.is(0, 1);
         // assert result by excepted value.
         assertThat(rsl, is(true));
