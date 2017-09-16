e84cfa149c3ebfedcd5971ffc57add6f239c3678
SirenOfOudeniz <ekaterina22_01_94@mail.ru>
2017-09-16 14:52:47 +0300

    commit Counter & CounterTest

---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FactorialTest {
+@Test
+public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
+    Factorial fac = new Factorial();
+    int result = fac.calc(5);
+    assertThat(result, is(120));
+}
+
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+       Factorial fac2 = new Factorial();
+       int result = fac2.calc(0);
+       assertThat(result,is(1));
+    }
+
+
+
+
+
+
+
+}
