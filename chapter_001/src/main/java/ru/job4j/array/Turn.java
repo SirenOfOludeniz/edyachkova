59f01cd790e148110756715616b1626c341305ff
SirenOfOludeniz <ekaterina22_01_94@mail.ru>
2017-09-21 14:15:30 +0300

    commit pravka (i < array.length/2)

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -3,23 +3,28 @@ package ru.job4j.array;
 import java.util.Arrays;
 
 public class Turn {
-   /* public static void main(String[] args) {
+  /*public static void main(String[] args) {
 
 
-        int[] nums = new int[5];
+        int[] nums = new int[9];
         nums[0] = 0;
         nums[1] = 1;
         nums[2] = 2;
         nums[3] = 3;
         nums[4] = 4;
+        nums[5] = 5;
+        nums[6] = 6;
+        nums[7] = 7;
+        nums[8] = 8;
 
 
-        System.out.println(Arrays.toString(back(nums)));
-    }*/
+
+        System.out.println(Arrays.toString(back(nums))); }  */
+
 
 
     public static int[] back(int[] array) {
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < array.length/2; i++) {
             int a;
             a = array[i];
             array[i] = ((array.length - 1) - i);
