489f711e693f919aa4dd535596e8e1ba39e37c8a
SirenOfOludeniz <ekaterina22_01_94@mail.ru>
2017-10-08 13:35:48 +0300

    WordInWord

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayWordInword.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+public class ArrayWordInword {
+   /* public static void main(String[] args) {
+        String fullword = "dinozavr";
+        String shortword = "dino";
+        System.out.println(contains(fullword,shortword));
+    }*/
+    public static boolean contains(String origin, String bun) {
+       char[] full = origin.toCharArray();
+       boolean a = false;
+
+        for (int i = 0; i < (origin.length()-bun.length())+1 ; i++) {
+            String fulw = new String(full,i, (bun.length()));
+            if(fulw.equals(bun)) {
+                a = true;
+                break;
+            }
+            else a = false;
+            System.out.println(fulw);
+        }
+
+        return a; }
+    }
+
+
