03c47c9fe54be1d3d40f04c814ad36d19ca66f18
SirenOfOudeniz <ekaterina22_01_94@mail.ru>
2017-10-26 12:51:39 +0300

    25 10 17 2

---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+
+    Counter brbr = new Counter();
+	int result = brbr.add(3,10);
+	assertThat(result, is(28));
+	
+	
+	
+	}
+}
