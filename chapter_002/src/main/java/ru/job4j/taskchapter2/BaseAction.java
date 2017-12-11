34650189100721f53d33ee1e6ad29cf4410516cb
SirenOfOludeniz <Ekaterina22_01_94@mail.ru>
2017-12-11 14:24:32 +0300

    BaseAction3

--- chapter_002/src/main/java/ru/job4j/taskchapter2/BaseAction.java
+++ chapter_002/src/main/java/ru/job4j/taskchapter2/BaseAction.java
@@ -1,11 +1,15 @@
 package ru.job4j.taskchapter2;
 
 public abstract class BaseAction implements UserAction {
+    String name;
+    int key;
+
 
     public BaseAction(String name, int key) { //такие же конструкторы нужно будет создать в классах наследниках
-
+        this.name = name;
+        this.key = key;
     }
 
-    public String info() {return "..."; }
+    public String info() {return name + key(); }
 
 }
