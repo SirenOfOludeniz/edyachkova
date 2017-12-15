ba7f3655f86b9c61baf0fdf6c374770f7cfd1c26
SirenOfOludeniz <Ekaterina22_01_94@mail.ru>
2017-12-15 20:17:32 +0300

    Chess диагональ RightUp

--- chapter_002/src/main/java/ru/job4j/ChessGame/Bishop.java
+++ chapter_002/src/main/java/ru/job4j/ChessGame/Bishop.java
@@ -8,18 +8,62 @@ public class Bishop extends Figure { //по условию нужен implements
     }
     public Cell[] way(Cell source, Cell dest) throws ImpossibleMoveException {
         Cell[] copyway = new Cell[dest.getX() - source.getX()];
+        int b1 = 0; int b2 = 0; int k1 = 1; int k2 = -1;
+        int beginx = 0; int beginy = 0;
+        int endx = 0; int endy = 0;
+        int arraylenght = 0;
+
         try {
-             /*Метод должен работать так. dest - задают ячейку куда следует пойти.
-             Если фигура может туда пойти.
-             то Вернуть массив ячеек. которые должна пройти фигура.
-              */
-        Cell[] cellway = new Cell[dest.getX() - source.getX()];
+            b1 = source.getY() - (k1 * source.getX()); // диагональ y = kx + b, b > 0 или b < 0 или b = 0, тогда y = x
+            b2 = source.getY() - (k2 * source.getX()); // диагональ y = (-k)x + b , здесь b всегда больше 0
+
+            // любая точка source лежит на таких двух диагоналях
+
+            //диагональ y = kx + b
+            if (b1 > 0) {
+              beginy = b1 + 1;
+              beginx = 1;
+            }
+            if (b1 < 0) {
+                beginx = Math.abs(b1) + 1;
+                beginy = 1;
+            }
+            if (b1 == 0) {
+                beginx = 1;
+                beginy = 1;
+            }
+
+            System.out.println("beginX :" + beginx);
+            System.out.println("beginY :" + beginy);
+
+            if (beginx == 1 && beginy != 1) {
+                arraylenght = 8 - beginy + 1;
+            }
+            if (beginx != 1 && beginy == 1) {
+                arraylenght = 8 - beginx + 1;
+            }
+            if (beginx == 1 && beginy == 1) {
+                arraylenght = 8;
+            }
+
+            System.out.println("Длина массива для диагонали RightUp :" + arraylenght);
+
+
+            Cell[] diagonalRightUp = new Cell[arraylenght];
+
+
+
+
+
+
+
+       /* Cell[] cellway = new Cell[dest.getX() - source.getX()]; // задали массив
         // количество клеток по диагонали, которые должен пройти слон куда-либо
             //совпадают с количеством клеток в проекции на любую из осей координат
             for (int i = 0; i < cellway.length ; i++) {
                 cellway[i] = copyway[i];
             }
-            // ГДЕ ПИСАТЬ САМУ ЛОГИКУ ТОГО, ЧТО СЛОН ХОДИТ ПО-ДИАГОНАЛИ?
+            // ГДЕ ПИСАТЬ САМУ ЛОГИКУ ТОГО, ЧТО СЛОН ХОДИТ ПО-ДИАГОНАЛИ?*/
 
 
             return copyway ; }
