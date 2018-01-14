bfd2aac0be178cc956024286a90b2251929a18de
SirenOfOludeniz <ekaterina22_01_94@mail.ru>
2018-01-14 15:10:21 +0300

    List convert is Map

--- chapter_003/src/main/java/ru/job4j/list_in_map/UserConvert.java
+++ chapter_003/src/main/java/ru/job4j/list_in_map/UserConvert.java
@@ -7,9 +7,15 @@ public class UserConvert {
 
     public HashMap<Integer, User> process(List<User> list) {
         HashMap<Integer, User> map = new HashMap<>();
-        for (int i = 0; i < list.size(); i++) {
-            map.put((list.get(i).getId()),(list.get(i)));
+//        for (int i = 0; i < list.size(); i++) {
+//            map.put((list.get(i).getId()),(list.get(i)));
+//        }
+        for (User user : list) {
+            map.put(user.getId(), user);
         }
+
+
+
         return map;
     }
 }
