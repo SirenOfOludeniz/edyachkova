dfd0404fbd8cbf26eebd3a63513fcb63d81c451a
SirenOfOludeniz <ekaterina22_01_94@mail.ru>
2018-01-21 12:42:28 +0300

    сортировка User by Comparator

--- chapter_003/src/main/java/ru/job4j/map_sort/SortUserM.java
+++ chapter_003/src/main/java/ru/job4j/map_sort/SortUserM.java
@@ -4,10 +4,6 @@ import java.util.*;
 public class SortUserM {
     public Set<UserM> sortByage (List<UserM> list) {
         Set<UserM> treeset = new TreeSet<>();
-
-//        for (int i = 0; i < list.size() ; i++) {
-//            treeset.add(list.get(i));
-//        }
         for (UserM userM : list) {
             treeset.add(list.get(list.indexOf(userM)));
         }
@@ -29,7 +25,6 @@ public class SortUserM {
         for (UserM userM : list1) {
             System.out.println("Name lenght " + list1.get(list1.indexOf(userM)).getName());
         }
-       // System.out.println("sortBy length of name " + list);
         return list1;
     }
     public List<UserM> sortByAllFields (List<UserM> list) {
@@ -37,25 +32,9 @@ public class SortUserM {
         list.sort(new Comparator<UserM>() {
             @Override
             public int compare(UserM o1, UserM o2) {
-               // int ageDifference = 0;
-                int ageDifference = o1.getAge() > o2.getAge() ? 1 : (o1.getAge() == o2.getAge() ? 0 : - 1);
-
-               /* if (o1.getAge() > o2.getAge()) {
-                    ageDifference = 1;
-                }
-                if (o1.getAge() == o2.getAge()) {
-                    ageDifference = 0;
-                }
-                if (o1.getAge() < o2.getAge()) {
-                    ageDifference = -1;
-                }*/
-                int result = 0;
+                int ageDifference = o1.getAge() - o2.getAge();
                 int nameCompare = o1.getName().compareTo(o2.getName());
-               // result = nameCompare !=0 ? nameCompare : Integer.compare(o1.getAge(), o2.getAge());
-                result = nameCompare != 0 ? nameCompare : ageDifference;
-
-//                agedif = (o1.getAge() - o2.getAge());
-//                return ((o1.getName().compareTo(o2.getName())) - agedif);
+                int result = nameCompare != 0 ? nameCompare : ageDifference;
                 return result;
             }
         });
