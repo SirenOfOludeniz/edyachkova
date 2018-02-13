b07da96c7d59575986fe7180ad4381aec7b54b40
SirenOfOludeniz <Ekaterina22_01_94@mail.ru>
2018-02-13 18:53:32 +0300

    Commit3

--- chapter_004/src/main/java/ru/job4j/test/GitRebaseBatch.java
+++ chapter_004/src/main/java/ru/job4j/test/GitRebaseBatch.java
@@ -4,6 +4,6 @@ public class GitRebaseBatch {
     public static void main(String[] args) {
         System.out.println("Commit 1...");
         System.out.println("Commit 2...");
-        System.out.println("Commit 3");
+        System.out.println("Commit 3...");
     }
 }
