83948e3557ed2ab932ec8d593d8711bec2bcecbc
SirenOfOludeniz <Ekaterina22_01_94@mail.ru>
2018-02-13 20:16:55 +0300

    Commit 2Git

--- chapter_004/src/main/java/ru/job4j/test/Git.java
+++ chapter_004/src/main/java/ru/job4j/test/Git.java
@@ -3,5 +3,6 @@ package ru.job4j.test;
 public class Git {
     public static void main(String[] args) {
         System.out.println("I try again..");
+        System.out.println("...and again.");
     }
 }
