cbed77009765b80c41b43a1ab362a5844b184072
SirenOfOludeniz <ekaterina22_01_94@mail.ru>
2018-03-26 01:08:04 +0300

    наблюдатель реализация Head First

---
+++ chapter_004/src/main/java/design/patterns/observer/DisplayElement.java
@@ -0,0 +1,5 @@
+package design.patterns.observer;
+
+public interface DisplayElement {
+    public void display();
+}
--- chapter_004/src/main/java/ru/job4j/service/Test.java
+++ chapter_004/src/main/java/ru/job4j/service/Test.java
@@ -4,22 +4,54 @@ import java.util.LinkedList;
 
 public class Test {
     public static void main(String[] args) {
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        linkedList.add(3);
-        linkedList.add(4);
-        linkedList.add(5);
-       // System.out.println("first output : " + linkedList);
-        linkedList.add(7);
-       // System.out.println("2nd output " + linkedList);
-        //System.out.println(linkedList.get(2));
-        linkedList.remove(1);
-        System.out.println(linkedList);
-       /* int[] array = {1,2,4,6};
-        int[] copy = new int[array.length];
-        System.arraycopy(array, 0, copy, 0, array.length);
-        for (int i = 0; i < copy.length; i++) {
-            System.out.println(copy[i]);
-        }*/
 
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
+        Whale whale = new Whale();
+        whale.printName();
+
+        Cow cow = new Whale();
+        cow.printName();
     }
 }
+class Cow{
+    public void printColor() {
+        System.out.println("Я = белая");
+    }
+    public void printName() {
+        System.out.println("Корова");
+    }
+
+}
+
+class Whale extends Cow{
+    public void printName() {
+        System.out.println("Я - кит");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
