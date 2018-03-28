af07f835139087edb17e17bad09982eb3ca451b5
SirenOfOludeniz <Ekaterina22_01_94@mail.ru>
2018-03-28 14:05:26 +0300

    pattern Decorator

---
+++ chapter_004/src/main/java/design/patterns/decorator/Mocha.java
@@ -0,0 +1,16 @@
+package design.patterns.decorator;
+
+public class Mocha extends Beverage{
+    Beverage beverage;
+
+    public Mocha(Beverage beverage) {
+        this.beverage = beverage;
+    }
+    public String getDescription() {
+        return beverage.getDescription() + ", Mocha";
+    }
+    @Override
+    public double cost() {
+        return .20 + beverage.cost();
+    }
+}
