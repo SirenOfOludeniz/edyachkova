3ce3da505d36af871149d86ea2f02bd3dff8d36b
SirenOfOludeniz <ekaterina22_01_94@mail.ru>
2018-03-29 14:48:09 +0300

    мой алг. двоич поиска, диапазон не только с 0

--- chapter_004/src/main/java/ru/job4j/test/TestDoubleFind.java
+++ chapter_004/src/main/java/ru/job4j/test/TestDoubleFind.java
@@ -3,11 +3,45 @@ package ru.job4j.test;
 
 
 public class TestDoubleFind {
+    /**
+     * //здесь поиск числа в диапазоне чисел от 0 до n
+     //если диапазон массива будет задан не с 0,
+     //то этот метод не будет работать
+     * @param array
+     * @param key
+     * @return
+     */
    public int doubleFind(int[] array, int key) {
+
        int downBound = 0;
        int upBound = array.length;
        int current = 0;
        while (current!= key) {
+           current = (downBound + upBound)/2;
+           if (current > key) {
+               upBound = current;
+           }
+           else {
+               downBound = current;
+               upBound = (current + current * 2)/2;
+
+           }
+       }
+       return current;
+   }
+
+    /**
+     * метод работает с отсортированным диапазоном чисел
+     * началом диапазона может быть любое число
+     * @param array
+     * @param key
+     * @return
+     */
+   public int doubleFindVariant2(int[] array, int key) {
+       int downBound = array[0];
+       int upBound = array[array.length - 1];
+       int current = 0;
+       while (current != key) {
            current = (downBound + upBound)/2;
            if (current > key) {
                upBound = current;
--- chapter_004/src/test/java/ru/job4j/test/TestDoubleFindTest.java
+++ chapter_004/src/test/java/ru/job4j/test/TestDoubleFindTest.java
@@ -16,5 +16,16 @@ public class TestDoubleFindTest {
         int expected = test.doubleFind(array, 9);
         assertThat(expected, is(9));
     }
+    @Test
+    public void find2Test() {
+        TestDoubleFind test = new TestDoubleFind();
+        int[] array = new int[50];
+        int index = 0;
+        for (int i = 50; i < 100; i++) {
+            array[index++] = i;
+        }
+        int expected = test.doubleFindVariant2(array, 65);
+        assertThat(expected, is(65));
+    }
 
 }
