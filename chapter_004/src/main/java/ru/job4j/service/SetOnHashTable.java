4f9e4802e4355df70162dd821668a980a22880d3
SirenOfOludeniz <Ekaterina22_01_94@mail.ru>
2018-04-03 17:57:44 +0300

    Set на хэш-таблице

---
+++ chapter_004/src/main/java/ru/job4j/service/SetOnHashTable.java
@@ -0,0 +1,85 @@
+package ru.job4j.service;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SetOnHashTable<E> implements Iterable<E>{
+    Object[] objects;
+    int index = 0;
+    int sizeArray;
+
+    public SetOnHashTable(int size) {
+        this.sizeArray = size;
+        this.objects = new Object[size];
+    }
+
+    public void overSize() {
+        int size;
+        Object[] copy;
+        if (this.index == this.objects.length) {
+            copy = this.objects;
+            size = this.objects.length * 2;
+            this.sizeArray = size;
+            this.objects = new Object[size];
+            System.arraycopy(copy, 0, this.objects, 0, copy.length);
+        }
+    }
+
+    public int hashFunction(int key) {
+        return key % this.objects.length;
+    }
+
+    public boolean add(E e) {
+        overSize();
+        boolean result = true;
+        if (contains(e)) {
+            this.objects[hashFunction(e.hashCode())] = e;
+            index++;
+        }
+        else result = false;
+        return result;
+    }
+
+
+    public boolean contains(E e) {
+        boolean result = true;
+       if (this.objects[hashFunction(e.hashCode())] != null && this.objects[hashFunction(e.hashCode())].equals(e)) {
+           result = false;
+       }
+        return result;
+    }
+
+    public boolean remove(E e) {
+        boolean result = false;
+        for (int i = 0; i < objects.length; i++) {
+            if (objects[i].equals(e)) {
+                objects[i] = null;
+                result = true;
+            }
+
+        }
+        return result;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+         int indexEl = 0;
+        return new Iterator<E>() {
+            @Override
+            public boolean hasNext() {
+                return next() != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                else {
+                    return (E) objects[index++];
+                }
+
+            }
+        };
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/service/SetOnHashTableTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.service;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+import static org.junit.Assert.*;
+
+public class SetOnHashTableTest {
+    @Test
+    public void setHashTableTest() {
+        SetOnHashTable<String>  words = new SetOnHashTable(10);
+        words.add("dog");
+        words.add("cat");
+        words.add("moon");
+        words.add("sun");
+        words.add("sea");
+        words.add("water");
+        words.add("moon");
+
+        for (int i = 0; i <words.sizeArray; i++) {
+            System.out.println(words.objects[i]);
+        }
+    }
+
+}
