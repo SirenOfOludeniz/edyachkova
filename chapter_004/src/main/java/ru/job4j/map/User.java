47ab0d21e828547e25de7757936a73f3184875b9
SirenOfOludeniz <Ekaterina22_01_94@mail.ru>
2018-04-11 21:58:57 +0300

    hash-code и  equals() переопределены

--- chapter_004/src/main/java/ru/job4j/map/User.java
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -6,8 +6,27 @@ public class User {
     String name;
     int children;
     Calendar birthday;
+
     public User(String name, Calendar birthday) {
         this.name = name;
         this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        return birthday != null ? birthday.equals(user.birthday) : user.birthday == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
+        return result;
     }
 }
