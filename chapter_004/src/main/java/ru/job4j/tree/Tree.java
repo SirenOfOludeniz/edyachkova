14a5bc614be02975760e8e12543dfe73796fc936
SirenOfOludeniz <Ekaterina22_01_94@mail.ru>
2018-04-21 14:11:50 +0300

    метод isBinary()

--- chapter_004/src/main/java/ru/job4j/tree/Tree.java
+++ chapter_004/src/main/java/ru/job4j/tree/Tree.java
@@ -60,6 +60,23 @@ public class Tree<E extends Comparable<E>> implements SimpleTree<E>{
         return rsl;
     }
 
+    public boolean isBinary() {
+        boolean result = true;
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> elem = data.poll();
+            if (elem.leaves().size() != 2) {
+                result = false;
+                break;
+            }
+            for (Node<E> child : elem.leaves()) {
+                data.offer(child);
+            }
+        }
+        return result;
+    }
+
     @Override
     public Iterator<E> iterator() {
 
