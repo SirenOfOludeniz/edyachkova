b2dbfc3e03dad3b28d7f220d99b5e33b2d723a96
SirenOfOludeniz <Ekaterina22_01_94@mail.ru>
2018-05-26 13:28:00 +0300

    Rectangle move попытка без копипасты2

--- chapter_005/src/main/java/ru/job4j/pingpong/RectangleMove.java
+++ chapter_005/src/main/java/ru/job4j/pingpong/RectangleMove.java
@@ -8,7 +8,7 @@ public class RectangleMove implements Runnable{
         this.rect = rect;
     }
 
-    @Override
+   /* @Override
     public void run() {
         while (true) {
             if (this.rect.getX() != 0 && this.rect.getX() != 300) {
@@ -23,23 +23,72 @@ public class RectangleMove implements Runnable{
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }
+    }*/
 
-            /*this.rect.setX(this.rect.getX() + 1);
-            if (this.rect.getX() == 300) {
-                while (this.rect.getX() > 0) {
-                    this.rect.setX(this.rect.getX() - 1);
+    public void run() {
+        while (true) {
+            if (this.rect.getX() != 0 && this.rect.getX() != 300) {
+                this.rect.setX(this.rect.getX() + 1);
+            }
+            if (this.rect.getY() != 0 && this.rect.getY() != 300) {
+                this.rect.setY(this.rect.getY() + 1);
+            }
+           if (this.rect.getX() == 0 && this.rect.getX() == 300) {
+                detectBorder(this.rect.getX(), this.rect.getY());
+                while (this.rect.getY() < 300 && this.rect.getY() > 0) {
+                    randomMoveY((int)(Math.random() * 10));
                     try {
-                        Thread.sleep(50);
+                        Thread.sleep(30);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
-            }
+           } else if (this.rect.getY() == 0 && this.rect.getY() == 300) {
+                detectBorder(this.rect.getY(), this.rect.getX());
+               while (this.rect.getX() < 300 && this.rect.getX() > 0) {
+                   randomMoveX((int)(Math.random() * 10));
+                   try {
+                       Thread.sleep(30);
+                   } catch (InterruptedException e) {
+                       e.printStackTrace();
+                   }
+               }
+           }
             try {
-                Thread.sleep(50);
+                Thread.sleep(30);
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }*/
+            }
+        }
+        
+    }
+
+    public void detectBorder(double coordinate, double randomCoord) {
+        if (coordinate == 0 ) {
+            int random = (int)(Math.random() * 10);
+            while (coordinate < 300 && randomCoord < 300 && randomCoord > 0) {
+                coordinate += 1;
+               // randomMoveCoordinate(random, randomCoord);
+                try {
+                    Thread.sleep(30);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+        if (coordinate == 300) {
+            int random = (int)(Math.random() * 10);
+            while (coordinate > 0 && randomCoord < 300 && randomCoord > 0) {
+                coordinate -= 1;
+               // randomMoveCoordinate(random, randomCoord);
+                try {
+                    Thread.sleep(30);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
@@ -113,6 +162,15 @@ public class RectangleMove implements Runnable{
             this.rect.setY(this.rect.getY() + 1);
         } else if (a > 8 && a <= 10) {
             this.rect.setY(this.rect.getY() + 0);
+        }
+    }
+    public void randomMoveCoordinate(int a, double coord) {
+        if (a >= 0 && a <= 4) {
+            coord -= 1;
+        } else if (a > 4 && a <= 8) {
+            coord += 1;
+        } else if (a > 8 && a <= 10) {
+            coord += 0;
         }
     }
 }
