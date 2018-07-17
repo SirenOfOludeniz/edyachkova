f37b6a77f684028313381d7e7c9db47f0e2c4a48
SirenOfOludeniz <mailto:Ekaterina22_01_94@mail.ru>
2018-07-17 14:22:45 +0300

    ThreadPool

--- chapter_005/src/main/java/ru/job4j/waitnotify/ThreadPool.java
+++ chapter_005/src/main/java/ru/job4j/waitnotify/ThreadPool.java
@@ -6,59 +6,76 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class ThreadPool {
     private final List<Thread> threads = new LinkedList<>();
     private final Queue<Runnable> tasks = new LinkedBlockingQueue<>();
+    private volatile boolean isRunning = true;
+
+    public ThreadPool(Thread A, Thread B) {
+        threads.add(new ThreadForPoolA(tasks));
+        threads.add(new ThreadForPoolB(tasks));
+    }
 
     public void work(Runnable job) {
-        tasks.add(job);
+        if (isRunning) {
+            tasks.offer(job);
+        }
+        if (this.tasks.size() == 1) {
+            notifyAll();
+        }
 
     }
 
     public void shutdown() {
-
+        this.isRunning = false;
     }
-}
-class SomeRunnableClassA implements Runnable {
-
-    @Override
-    public void run() {
-
-    }
-}
-class SomeRunnableClassB implements Runnable {
-
-    @Override
-    public void run() {
-
-    }
-}
 
 
+    private final class ThreadForPoolA extends Thread {
+        final Queue<Runnable> tasks;
 
+        public ThreadForPoolA(final Queue<Runnable> queue) {
+            this.tasks = queue;
+        }
 
-class ThreadForPoolA extends Thread{
-    final Queue<Runnable> tasks;
-    public ThreadForPoolA(final LinkedBlockingQueue<Runnable> queue) {
-        this.tasks = queue;
-    }
-    public void run() {
-        for (Runnable runnable : tasks) {
-            runnable.run();
+        public void run() {
+            if (this.tasks.isEmpty()) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    System.out.println("Interrupted");
+                }
+            }
+            while (!tasks.isEmpty() && isRunning) {
+                Runnable task = tasks.poll();
+                if (task != null) {
+                    task.run();
+                }
+            }
+
         }
     }
-}
 
 
+    private final class ThreadForPoolB extends Thread {
+        final Queue<Runnable> tasks;
 
+        public ThreadForPoolB(final Queue<Runnable> queue) {
+            this.tasks = queue;
+        }
 
+        public void run() {
+            if (this.tasks.isEmpty()) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    System.out.println("Interrupted");
+                }
+            }
+            while (!tasks.isEmpty() && isRunning) {
+                Runnable task = tasks.poll();
+                if (task != null) {
+                    task.run();
+                }
+            }
 
-
-class ThreadForPoolB extends Thread{
-    final Queue<Runnable> tasks;
-    public ThreadForPoolB(final LinkedBlockingQueue<Runnable> queue) {
-        this.tasks = queue;
-    }
-    public void run() {
-        for (Runnable runnable : tasks) {
-            runnable.run();
         }
     }
 }
