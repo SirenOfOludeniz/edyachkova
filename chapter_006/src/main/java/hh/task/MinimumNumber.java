302852a820f3d161ae84cd8c7b98f5c0dd3e8f4f
SirenOfOludeniz <mailto:Ekaterina22_01_94@mail.ru>
2018-09-17 16:15:29 +0300

    test task найти миним. пропущенное число

--- chapter_006/src/main/java/hh/task/MinimumNumber.java
+++ chapter_006/src/main/java/hh/task/MinimumNumber.java
@@ -1,19 +1,17 @@
 package hh.task;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class MinimumNumber {
     public static void main(String[] args) throws IOException{
-        MinimumNumber minimumNumber = new MinimumNumber();
-
-       String array = "-200 -100 1 8 4";
-        System.out.println(minimumNumber.findMinimumNumber(array));
-
-
-
-
+        MinimumNumber minNum = new MinimumNumber();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input = reader.readLine();
+        minNum.findMinimumNumber(input);
     }
     public int findMinimumNumber(String source) {
         String[] numbers = source.split(" ");
